f3f9f8b3620fbfd7b130d907bec9a21560085a02
Souvik Biswas <sbis1999@gmail.com>
2020-09-18 18:22:47 +0530

    Add custom colors

---
+++ lib/resources/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const Color dark_blue = Color(0xFF05008b);
+  static const Color dark_cyan = Color(0xFF025ab5);
+  static const Color sea_blue = Color(0xFF106db6);
+  static const Color neon_green = Color(0xFF51ca98);
+}
