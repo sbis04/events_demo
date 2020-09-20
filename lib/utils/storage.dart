e34da0d425df9eb2d4d17f9eab8be8a159364093
Souvik Biswas <sbis1999@gmail.com>
2020-09-20 07:46:12 +0530

    Storage method

--- lib/utils/storage.dart
+++ lib/utils/storage.dart
@@ -5,8 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-final CollectionReference mainCollection =
-    FirebaseFirestore.instance.collection('event');
+final CollectionReference mainCollection = FirebaseFirestore.instance.collection('event');
 
 final DocumentReference documentReference = mainCollection.doc('test');
 
@@ -15,22 +14,29 @@ class Storage {
     @required String id,
     @required String name,
     @required String description,
+    @required String location,
+    @required String link,
+    @required List<String> attendeeEmails,
+    @required bool shouldNotifyAttendees,
+    @required bool hasConfereningSupport,
     @required int startTimeInEpoch,
     @required int endTimeInEpoch,
-    @required int numberOfAttedees,
   }) async {
     // TODO: Initialize it only once during the starting of the app
     await Firebase.initializeApp();
-    DocumentReference documentReferencer =
-        documentReference.collection('events').doc(id);
+    DocumentReference documentReferencer = documentReference.collection('events').doc(id);
 
     Map<String, dynamic> data = <String, dynamic>{
       "id": id,
       "name": name,
       "desc": description,
+      "loc": location,
+      "link": link,
+      "emails": attendeeEmails,
+      "should_notify": shouldNotifyAttendees,
+      "has_conferencing": hasConfereningSupport,
       "start": startTimeInEpoch,
       "end": endTimeInEpoch,
-      "attendees": numberOfAttedees,
     };
     print('DATA:\n$data');
 
