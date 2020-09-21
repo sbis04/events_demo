f7f2608eed1bef062e47cf908994c6a7c9d16859
Souvik Biswas <sbis1999@gmail.com>
2020-09-21 23:16:12 +0530

    Add method for updating event

--- lib/utils/storage.dart
+++ lib/utils/storage.dart
@@ -43,6 +43,39 @@ class Storage {
     }).catchError((e) => print(e));
   }
 
+  Future<void> updateEventData({
+    @required String id,
+    @required String name,
+    @required String description,
+    @required String location,
+    @required String link,
+    @required List<String> attendeeEmails,
+    @required bool shouldNotifyAttendees,
+    @required bool hasConfereningSupport,
+    @required int startTimeInEpoch,
+    @required int endTimeInEpoch,
+  }) async {
+    DocumentReference documentReferencer = documentReference.collection('events').doc(id);
+
+    Map<String, dynamic> data = <String, dynamic>{
+      "id": id,
+      "name": name,
+      "desc": description,
+      "loc": location,
+      "link": link,
+      "emails": attendeeEmails,
+      "should_notify": shouldNotifyAttendees,
+      "has_conferencing": hasConfereningSupport,
+      "start": startTimeInEpoch,
+      "end": endTimeInEpoch,
+    };
+    print('DATA:\n$data');
+
+    await documentReferencer.update(data).whenComplete(() {
+      print("Event updated in the database, id: {$id}");
+    }).catchError((e) => print(e));
+  }
+
   Future<void> deleteEvent({@required String id}) async {
     DocumentReference documentReferencer = documentReference.collection('events').doc(id);
 
