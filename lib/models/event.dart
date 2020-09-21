24c8d6c3a04200491f28556d1290d44fa2e9fa71
Souvik Biswas <sbis1999@gmail.com>
2020-09-21 19:19:17 +0530

    Add model class for handling events

---
+++ lib/models/event.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Event {
+  final String id;
+  final String name;
+  final String description;
+  final String location;
+  final String link;
+  final List<dynamic> attendeeEmails;
+  final bool shouldNotifyAttendees;
+  final bool hasConfereningSupport;
+  final int startTimeInEpoch;
+  final int endTimeInEpoch;
+
+  Event({
+    @required this.id,
+    @required this.name,
+    @required this.description,
+    @required this.location,
+    @required this.link,
+    @required this.attendeeEmails,
+    @required this.shouldNotifyAttendees,
+    @required this.hasConfereningSupport,
+    @required this.startTimeInEpoch,
+    @required this.endTimeInEpoch,
+  });
+
+  Event.fromMap(Map snapshot)
+      : id = snapshot['id'] ?? '',
+        name = snapshot['name'] ?? '',
+        description = snapshot['desc'],
+        location = snapshot['loc'],
+        link = snapshot['link'],
+        attendeeEmails = snapshot['emails'] ?? '',
+        shouldNotifyAttendees = snapshot['should_notify'],
+        hasConfereningSupport = snapshot['has_conferencing'],
+        startTimeInEpoch = snapshot['start'],
+        endTimeInEpoch = snapshot['end'];
+
+  toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'desc': description,
+      'loc': location,
+      'link': link,
+      'emails': attendeeEmails,
+      'should_notify': shouldNotifyAttendees,
+      'has_conferencing': hasConfereningSupport,
+      'start': startTimeInEpoch,
+      'end': endTimeInEpoch,
+    };
+  }
+}
