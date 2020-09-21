e3d7be886cc9fe15ba27b747237575db811908af
Souvik Biswas <sbis1999@gmail.com>
2020-09-21 22:24:53 +0530

    Add dashboard screen

---
+++ lib/screens/dashboard_screen.dart
@@ -0,0 +1,201 @@
+import 'package:events_demo/models/event.dart';
+import 'package:events_demo/resources/color.dart';
+import 'package:events_demo/screens/create_screen.dart';
+import 'package:events_demo/utils/storage.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DashboardScreen extends StatefulWidget {
+  @override
+  _DashboardScreenState createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  Storage storage = Storage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(
+          color: Colors.grey, //change your color here
+        ),
+        title: Text(
+          'Event Details',
+          style: TextStyle(
+            color: CustomColor.dark_blue,
+            fontSize: 22,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: CustomColor.dark_cyan,
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => CreateScreen(),
+            ),
+          );
+        },
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          left: 16.0,
+          right: 16.0,
+          bottom: 30.0,
+        ),
+        child: Container(
+          padding: EdgeInsets.only(top: 8.0),
+          color: Colors.white,
+          child: StreamBuilder(
+            stream: storage.retrieveEvents(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (context, index) {
+                    Map<String, dynamic> eventInfo = snapshot.data.documents[index].data();
+
+                    Event event = Event.fromMap(eventInfo);
+
+                    DateTime startTime = DateTime.fromMillisecondsSinceEpoch(event.startTimeInEpoch);
+                    DateTime endTime = DateTime.fromMillisecondsSinceEpoch(event.endTimeInEpoch);
+
+                    String startTimeString = DateFormat.jm().format(startTime);
+                    String endTimeString = DateFormat.jm().format(endTime);
+                    String dateString = DateFormat.yMMMMd().format(startTime);
+
+                    return InkWell(
+                      onTap: () {
+                        // Navigator.of(context).push(
+                        //   MaterialPageRoute(
+                        //     builder: (context) => EditClassScreen(
+                        //       // date: dateString,
+                        //       id: id,
+                        //       documentName: docName,
+                        //       startTime: startTime,
+                        //       endTime: endTime,
+                        //       subject: subject,
+                        //       description: description,
+                        //       link: link,
+                        //       year: year,
+                        //     ),
+                        //   ),
+                        // );
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Container(
+                            padding: EdgeInsets.only(
+                              bottom: 16.0,
+                              top: 16.0,
+                              left: 16.0,
+                              right: 16.0,
+                            ),
+                            decoration: BoxDecoration(
+                              color: CustomColor.neon_green.withOpacity(0.3),
+                              borderRadius: BorderRadius.circular(12.0),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  event.name,
+                                  style: TextStyle(
+                                    color: CustomColor.dark_blue,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 22,
+                                    letterSpacing: 1,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Text(
+                                  event.description,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    color: Colors.black38,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                    letterSpacing: 1,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                                  child: Text(
+                                    event.link,
+                                    style: TextStyle(
+                                      color: CustomColor.dark_blue.withOpacity(0.5),
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                      letterSpacing: 0.5,
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Container(
+                                      height: 50,
+                                      width: 5,
+                                      color: CustomColor.neon_green,
+                                    ),
+                                    SizedBox(width: 10),
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          dateString,
+                                          style: TextStyle(
+                                            color: CustomColor.dark_cyan,
+                                            fontFamily: 'OpenSans',
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16,
+                                            letterSpacing: 1.5,
+                                          ),
+                                        ),
+                                        Text(
+                                          '$startTimeString - $endTimeString',
+                                          style: TextStyle(
+                                            color: CustomColor.dark_cyan,
+                                            fontFamily: 'OpenSans',
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16,
+                                            letterSpacing: 1.5,
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              }
+              return Center(
+                child: CircularProgressIndicator(
+                  strokeWidth: 2,
+                  valueColor: AlwaysStoppedAnimation<Color>(CustomColor.sea_blue),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
