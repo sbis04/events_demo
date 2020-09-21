ef2dae1d060307b01246c7fd5c1ae7a46cc38886
Souvik Biswas <sbis1999@gmail.com>
2020-09-21 19:18:56 +0530

    Initialize Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:events_demo/screens/create_screen.dart';
+import 'package:events_demo/screens/dashboard_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:io' show Platform;
 
-void main() {
+Future<void> main() async {
   if (Platform.isAndroid) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Colors.white,
       statusBarIconBrightness: Brightness.dark,
     ));
   }
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   runApp(MyApp());
 }
@@ -26,7 +30,7 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.light,
         ),
       ),
-      home: CreateScreen(),
+      home: DashboardScreen(),
     );
   }
 }
