54788a12fb980ecf8c2367c4e7f9f77619999059
Souvik Biswas <sbis1999@gmail.com>
2020-09-23 00:46:11 +0530

    Remove unwanted package imports

--- lib/utils/storage.dart
+++ lib/utils/storage.dart
@@ -1,9 +1,5 @@
-import 'dart:io';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 final CollectionReference mainCollection = FirebaseFirestore.instance.collection('event');
 
