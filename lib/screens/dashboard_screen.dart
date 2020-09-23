841e5277d0823cd04c1bfd44f2a1448baa8a1e82
Souvik Biswas <sbis1999@gmail.com>
2020-09-23 09:45:04 +0530

    Fix dashboard padding

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -58,6 +58,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
               if (snapshot.hasData) {
                 if (snapshot.data.documents.length > 0) {
                   return ListView.builder(
+                    physics: BouncingScrollPhysics(),
                     itemCount: snapshot.data.documents.length,
                     itemBuilder: (context, index) {
                       Map<String, dynamic> eventInfo = snapshot.data.documents[index].data();
@@ -71,108 +72,111 @@ class _DashboardScreenState extends State<DashboardScreen> {
                       String endTimeString = DateFormat.jm().format(endTime);
                       String dateString = DateFormat.yMMMMd().format(startTime);
 
-                      return InkWell(
-                        onTap: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) => EditScreen(event: event),
-                            ),
-                          );
-                        },
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Container(
-                              padding: EdgeInsets.only(
-                                bottom: 16.0,
-                                top: 16.0,
-                                left: 16.0,
-                                right: 16.0,
+                      return Padding(
+                        padding: EdgeInsets.only(bottom: 16.0),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => EditScreen(event: event),
                               ),
-                              decoration: BoxDecoration(
-                                color: CustomColor.neon_green.withOpacity(0.3),
-                                borderRadius: BorderRadius.circular(12.0),
-                              ),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    event.name,
-                                    style: TextStyle(
-                                      color: CustomColor.dark_blue,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 22,
-                                      letterSpacing: 1,
-                                    ),
-                                  ),
-                                  SizedBox(height: 10),
-                                  Text(
-                                    event.description,
-                                    maxLines: 2,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(
-                                      color: Colors.black38,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 16,
-                                      letterSpacing: 1,
-                                    ),
-                                  ),
-                                  SizedBox(height: 10),
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
-                                    child: Text(
-                                      event.link,
+                            );
+                          },
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.only(
+                                  bottom: 16.0,
+                                  top: 16.0,
+                                  left: 16.0,
+                                  right: 16.0,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: CustomColor.neon_green.withOpacity(0.3),
+                                  borderRadius: BorderRadius.circular(12.0),
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      event.name,
                                       style: TextStyle(
-                                        color: CustomColor.dark_blue.withOpacity(0.5),
+                                        color: CustomColor.dark_blue,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 22,
+                                        letterSpacing: 1,
+                                      ),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      event.description,
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                        color: Colors.black38,
                                         fontWeight: FontWeight.bold,
                                         fontSize: 16,
-                                        letterSpacing: 0.5,
+                                        letterSpacing: 1,
                                       ),
                                     ),
-                                  ),
-                                  SizedBox(height: 10),
-                                  Row(
-                                    crossAxisAlignment: CrossAxisAlignment.center,
-                                    children: [
-                                      Container(
-                                        height: 50,
-                                        width: 5,
-                                        color: CustomColor.neon_green,
+                                    SizedBox(height: 10),
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                                      child: Text(
+                                        event.link,
+                                        style: TextStyle(
+                                          color: CustomColor.dark_blue.withOpacity(0.5),
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16,
+                                          letterSpacing: 0.5,
+                                        ),
                                       ),
-                                      SizedBox(width: 10),
-                                      Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            dateString,
-                                            style: TextStyle(
-                                              color: CustomColor.dark_cyan,
-                                              fontFamily: 'OpenSans',
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 16,
-                                              letterSpacing: 1.5,
+                                    ),
+                                    SizedBox(height: 10),
+                                    Row(
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        Container(
+                                          height: 50,
+                                          width: 5,
+                                          color: CustomColor.neon_green,
+                                        ),
+                                        SizedBox(width: 10),
+                                        Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              dateString,
+                                              style: TextStyle(
+                                                color: CustomColor.dark_cyan,
+                                                fontFamily: 'OpenSans',
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 16,
+                                                letterSpacing: 1.5,
+                                              ),
                                             ),
-                                          ),
-                                          Text(
-                                            '$startTimeString - $endTimeString',
-                                            style: TextStyle(
-                                              color: CustomColor.dark_cyan,
-                                              fontFamily: 'OpenSans',
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 16,
-                                              letterSpacing: 1.5,
+                                            Text(
+                                              '$startTimeString - $endTimeString',
+                                              style: TextStyle(
+                                                color: CustomColor.dark_cyan,
+                                                fontFamily: 'OpenSans',
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 16,
+                                                letterSpacing: 1.5,
+                                              ),
                                             ),
-                                          ),
-                                        ],
-                                      )
-                                    ],
-                                  ),
-                                ],
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       );
                     },
