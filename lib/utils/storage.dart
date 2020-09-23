067d4766507b5d895d1c62a421c0c1cab17eee88
Souvik Biswas <sbis1999@gmail.com>
2020-09-23 12:27:39 +0530

    Update using event object

--- lib/utils/storage.dart
+++ lib/utils/storage.dart
@@ -1,74 +1,32 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:events_demo/models/event_info.dart';
 import 'package:flutter/material.dart';
 
 final CollectionReference mainCollection = FirebaseFirestore.instance.collection('event');
-
 final DocumentReference documentReference = mainCollection.doc('test');
 
 class Storage {
-  Future<void> storeEventData({
-    @required String id,
-    @required String name,
-    @required String description,
-    @required String location,
-    @required String link,
-    @required List<String> attendeeEmails,
-    @required bool shouldNotifyAttendees,
-    @required bool hasConfereningSupport,
-    @required int startTimeInEpoch,
-    @required int endTimeInEpoch,
-  }) async {
-    DocumentReference documentReferencer = documentReference.collection('events').doc(id);
+  Future<void> storeEventData(EventInfo eventInfo) async {
+    DocumentReference documentReferencer = documentReference.collection('events').doc(eventInfo.id);
 
-    Map<String, dynamic> data = <String, dynamic>{
-      "id": id,
-      "name": name,
-      "desc": description,
-      "loc": location,
-      "link": link,
-      "emails": attendeeEmails,
-      "should_notify": shouldNotifyAttendees,
-      "has_conferencing": hasConfereningSupport,
-      "start": startTimeInEpoch,
-      "end": endTimeInEpoch,
-    };
+    Map<String, dynamic> data = eventInfo.toJson();
+
     print('DATA:\n$data');
 
     await documentReferencer.set(data).whenComplete(() {
-      print("Event added to the database, id: {$id}");
+      print("Event added to the database, id: {${eventInfo.id}}");
     }).catchError((e) => print(e));
   }
 
-  Future<void> updateEventData({
-    @required String id,
-    @required String name,
-    @required String description,
-    @required String location,
-    @required String link,
-    @required List<String> attendeeEmails,
-    @required bool shouldNotifyAttendees,
-    @required bool hasConfereningSupport,
-    @required int startTimeInEpoch,
-    @required int endTimeInEpoch,
-  }) async {
-    DocumentReference documentReferencer = documentReference.collection('events').doc(id);
+  Future<void> updateEventData(EventInfo eventInfo) async {
+    DocumentReference documentReferencer = documentReference.collection('events').doc(eventInfo.id);
 
-    Map<String, dynamic> data = <String, dynamic>{
-      "id": id,
-      "name": name,
-      "desc": description,
-      "loc": location,
-      "link": link,
-      "emails": attendeeEmails,
-      "should_notify": shouldNotifyAttendees,
-      "has_conferencing": hasConfereningSupport,
-      "start": startTimeInEpoch,
-      "end": endTimeInEpoch,
-    };
+    Map<String, dynamic> data = eventInfo.toJson();
+
     print('DATA:\n$data');
 
     await documentReferencer.update(data).whenComplete(() {
-      print("Event updated in the database, id: {$id}");
+      print("Event updated in the database, id: {${eventInfo.id}}");
     }).catchError((e) => print(e));
   }
 
